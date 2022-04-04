411563e402558d431294ae19613097bb9a1aa263
Brad Schmitt <bschmitt@newrelic.com>
2022-04-03 21:32:38 -0600

    update examples

--- examples/main.ts
+++ examples/main.ts
@@ -1,51 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
-import * as ssp from '@aws-quickstart/eks-blueprints';
-import { NewRelicAddOn } from '@newrelic/newrelic-eks-blueprints-addon';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+// import { NewRelicAddOn } from '@newrelic/newrelic-eks-blueprints-addon';
+import { NewRelicAddOn } from '../../dist';
+import { Construct } from "constructs";
 
-const app = new cdk.App();
 
-ssp.EksBlueprint.builder()
-    .addOns(new ssp.MetricsServerAddOn)
-    .addOns(new ssp.ClusterAutoScalerAddOn)
-    .addOns(new ssp.addons.SSMAgentAddOn)
-    .addOns(new ssp.addons.SecretsStoreAddOn)
-    .addOns(new NewRelicAddOn({
-        version: "4.2.0-beta",
-        newRelicClusterName: "demo-cluster",
-        awsSecretName: "newrelic-pixie-combined", // Secret Name in AWS Secrets Manager
-        installPixie: true,
-        installPixieIntegration: true,
-        /**
-         * Examples:
-         * nri-prometheus: configuring metric exclusions
-         * newrelic-infrastructure: enabling cluster node process metrics
-         */
-        values: {
-            "nri-prometheus": {
-               "config": {
-                  "transformations": [
-                     {
-                        "description": "Prometheus metric exclusion example",
-                        "ignore_metrics": [
-                           {
-                              "prefixes": [
-                                 "kube_"
-                              ]
-                           }
-                        ]
-                     }
-                  ]
-               }
-            },
-            "newrelic-infrastructure": {
-               "common": {
-                  "agentConfig": {
-                     "enable_process_metrics": true
-                  }
-               }
-            }
-         }
-    }))
-    .region(process.env.AWS_REGION)
-    .account(process.env.AWS_ACCOUNT)
-    .build(app, 'demo-cluster');
+export default class NewRelicConstruct extends Construct {
+    constructor(scope: Construct, id: string, props: cdk.StackProps) {
+        super(scope, id);
+        // AddOns for the cluster
+        const stackId = `${id}-blueprint`;
+
+        const addOns: Array<blueprints.ClusterAddOn> = [
+            new blueprints.addons.SecretsStoreAddOn(),
+            new NewRelicAddOn({
+                version: "4.2.0-beta",
+                newRelicClusterName: id,
+                // Uncomment "awsSecretName" after you create your secret in AWS Secrets Manager.
+                // Required: nrLicenseKey
+                // Optional: pixieDeployKey, pixieApiKey
+                //
+                // Format:
+                // {
+                //     "pixieDeployKey": "px-dep-XXXX",
+                //     "pixieApiKey": "px-api-XXXX",
+                //     "nrLicenseKey": "XXXXNRAL"
+                // }
+                awsSecretName: "newrelic-pixie-combined",
+                //
+                // Uncomment "installPixie" and "installPixieIntegration" if installing Pixie.
+                installPixie: true,
+                installPixieIntegration: true,
+                //
+                // For additional install options, visit the New Relic addon docs:
+                // https://github.com/newrelic-experimental/newrelic-eks-blueprints-addon
+            })
+        ];
+
+        blueprints.EksBlueprint.builder()
+            .addOns(...addOns)
+            .build(scope, stackId, props);
+    }
+}
